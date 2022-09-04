dafa2c5a71e3103f523cc5c68b8bb519b5ac4aa4
rohitgulam <rohitsgulam@gmail.com>
2022-09-04 16:51:14 +0300

    added CRUD routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products', function(){
-    return 'products';
-});
+// Route::get('/products', [ProductController::class, 'index']);
+ 
+// Route::post('/products', [ProductController::class, 'store']);
+
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+
+// Works in the same way as creating routes but it is easier if you just have a basic CRUD api
+Route::resource('products', ProductController::class);
